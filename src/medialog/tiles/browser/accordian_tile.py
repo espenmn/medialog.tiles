fc47cbde9b19983d3c7f5273d00b77201600aa87
Espen Moe-Nilssen <espen@medialog.no>
2017-02-14 11:08:37 +0100

    not working still

--- src/medialog/tiles/browser/accordian_tile.py
+++ src/medialog/tiles/browser/accordian_tile.py
@@ -1,21 +1,35 @@
-from plone.app.vocabularies.catalog import CatalogSource
-from plone.supermodel import model
-from zope import schema
+# -*- coding: utf-8 -*-
 
+from plone import api
 from plone.app.tiles.browser.add import DefaultAddForm
 from plone.app.tiles.browser.add import DefaultAddView
 from plone.app.tiles.browser.edit import DefaultEditForm
 from plone.app.tiles.browser.edit import DefaultEditView
-
+from plone.memoize.view import memoize
+from plone.supermodel import model
+from plone.directives import form
 from plone.tiles import Tile
 from plone.tiles.data import TransientTileDataManager
 from plone.tiles.interfaces import ITileDataManager
+from zope import schema
+from zope.i18nmessageid import MessageFactory
+#from zope.interface import provider
+
+
+#????
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.publisher.browser import BrowserView
+from zope.schema import getFields
+from plone.tiles.interfaces import ITileType
+
+
+_ = MessageFactory('medialog.iconpicker')
+
 
 class IAccordianTile(model.Schema):
-   content_uid = schema.Choice(
+   sometext = schema.Text(
    title=u"Select an object",   # XXX replace this with a message factory
    required=True,
-   source=CatalogSource(portal_type=['Folder',]),
    )
    
 class AccordianTile(Tile):
