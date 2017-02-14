09cbe8768f6da7354a7fe5f398695aaa54fa035c
Espen Moe-Nilssen <espen@medialog.no>
2017-02-14 23:59:57 +0100

    Update tiles.py

--- src/medialog/tiles/browser/tiles.py
+++ src/medialog/tiles/browser/tiles.py
@@ -49,7 +49,7 @@ class ILRTile(model.Schema):
                       default="Float left or right?"),
     )
     
-    body = RichText(title = _(u"body_text", default=u"Body text"),
+    body = TextLine(title = _(u"body_text", default=u"Body text"),
         required = False,
         description = _("body_icon",
                       default="Body text"),
