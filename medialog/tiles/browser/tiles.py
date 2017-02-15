0acff13fa019e4daa80843bfe69bae39656aa983
Espen Moe-Nilssen <espen@medialog.no>
2017-02-15 18:46:55 +0100

    adding py files

---
+++ medialog/tiles/browser/tiles.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+from plone import api
+from plone.app.tiles.browser.add import DefaultAddForm
+from plone.app.tiles.browser.add import DefaultAddView
+from plone.app.tiles.browser.edit import DefaultEditForm
+from plone.app.tiles.browser.edit import DefaultEditView
+from plone.memoize.view import memoize
+from plone.supermodel import model
+from plone.directives import form
+from plone.tiles import Tile
+from plone.tiles.data import TransientTileDataManager
+from plone.tiles.interfaces import ITileDataManager
+from zope import schema
+from zope.i18nmessageid import MessageFactory
+#from zope.interface import provider
+
+
+#????
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.publisher.browser import BrowserView
+from zope.schema import getFields
+from plone.tiles.interfaces import ITileType
+
+
+from collective.z3cform.datagridfield import DataGridFieldFactory 
+from collective.z3cform.datagridfield import DictRow
+
+
+_ = MessageFactory('medialog.tiles')
+
+ 
+ 
+
+
+ 
+    
+class IMultiTile(model.Schema):
+    
+    css_class =schema.TextLine(
+        title = _("css class", default=u"CSS class"),
+        required = False,
+        description = _("help_css_class",
+                      default="CSS Class"),
+    )
+    
+    
+class MultiTile(Tile):
+    """A tile that displays image and richtext"""
+
+    def __init__(self, context, request):
+        super(MultiTile, self).__init__(context, request)
+        
+    @property
+    def data(self):
+        data = super(MultiTile, self).data
+        return data
+        
+     
---
+++ medialog/tiles/tests.py
@@ -0,0 +1,66 @@
+import doctest
+import unittest
+
+from Testing import ZopeTestCase as ztc
+
+from Products.Five import zcml
+from Products.PloneTestCase import PloneTestCase as ptc
+from Products.PloneTestCase.layer import PloneSite
+from Products.PloneTestCase.layer import onsetup
+
+import medialog.tiles
+
+OPTION_FLAGS = doctest.NORMALIZE_WHITESPACE | \
+               doctest.ELLIPSIS
+
+ptc.setupPloneSite(products=['medialog.tiles'])
+
+
+class TestCase(ptc.PloneTestCase):
+
+    class layer(PloneSite):
+
+        @classmethod
+        def setUp(cls):
+            zcml.load_config('configure.zcml',
+              medialog.tiles)
+
+        @classmethod
+        def tearDown(cls):
+            pass
+
+
+def test_suite():
+    return unittest.TestSuite([
+
+        # Unit tests
+        #doctestunit.DocFileSuite(
+        #    'README.txt', package='medialog.tiles',
+        #    setUp=testing.setUp, tearDown=testing.tearDown),
+
+        #doctestunit.DocTestSuite(
+        #    module='medialog.tiles.mymodule',
+        #    setUp=testing.setUp, tearDown=testing.tearDown),
+
+
+        # Integration tests that use PloneTestCase
+        ztc.ZopeDocFileSuite(
+            'INTEGRATION.txt',
+            package='medialog.tiles',
+            optionflags = OPTION_FLAGS,
+            test_class=TestCase),
+
+        # -*- extra stuff goes here -*-
+
+        # Integration tests for Hyperlapse
+        ztc.ZopeDocFileSuite(
+            'Tiles.txt',
+            package='medialog.tiles',
+            optionflags = OPTION_FLAGS,
+            test_class=TestCase),
+
+
+        ])
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
