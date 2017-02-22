86dc08893be0265275dde48606ebd9ed2920fdd7
Espen Moe-Nilssen <espen@medialog.no>
2017-02-22 13:07:16 +0100

    CSS classes for multiple

--- medialog/tiles/browser/tiles.py
+++ medialog/tiles/browser/tiles.py
@@ -57,7 +57,7 @@ class IMultiTile(model.Schema):
         required = False,
         description = _("help_css_class",
                       default="CSS Class"),
-        values=("left", "right"),
+        values=("left", "right", "left dark", "right dark", "text-on-image"),
     )
     
     body = RichText(title=u"Rich text",
