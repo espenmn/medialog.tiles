24d056b8bd5d54559ee7b003aeaafc07c27f5d7d
Espen Moe-Nilssen <espen@medialog.no>
2017-02-27 15:47:59 +0100

    remove pdb

--- medialog/tiles/browser/tiles.py
+++ medialog/tiles/browser/tiles.py
@@ -237,7 +237,6 @@ class PicturesTile(MultiTile):
     """A tile that displays pictures"""
     
     def get_images(self):
-        import pdb; pdb.set_trace()
         catalog = api.portal.get_tool(name='portal_catalog')
         tagged_images = catalog(portal_type='Image', Subject=self.data['tags'], sort_on='id')
         return [image.getObject()for image in tagged_images]
